ba7968282e25218698b11b5e6c7cbb693e3a675e
Ashiq121914 <ashiq121914@gmail.com>
2024-09-12 18:00:50 +0600

    starting the project with app.js and mongodb connection

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const port = 5001;
+
+const url = "mongodb://localhost:27017/";
+const dbName = "studentDB";
+
+let db = null;
+
+// connect to mongoDB
+const connectToDb = async () => {
+  const client = new MongoClient(url);
+  await client.connect();
+  db = client.db(dbName);
+  console.log("connect to mongoDb");
+  return db;
+};
+
+app.use(bodyParser.json()); // Parses incoming requests with JSON payloads.
+
+connectToDb()
+  .then((database) => {
+    app.use((req, res, next) => {
+      req.db = database;
+      next();
+    });
+    // routes
+  })
+  .catch((error) => {
+    console.log("failed to connect ot mongoDb", error);
+  });
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
